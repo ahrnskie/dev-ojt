d9c37ccd5701abaa1bf289683c0d498662f47ea8
ahrnskie <ahrnex@gmail.com>
2024-06-25 09:42:55 +0800

    Added dev-prompts

--- dashboard.php
+++ dashboard.php
@@ -40,13 +40,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Offcanvas navbar</a>
+    <a class="navbar-brand" href="#">Employee Dashboard</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Employee Dashboard</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -141,8 +141,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                       <?php
                         // Free result set
                         unset($result);
+                      } else {
+                        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                      } else {         
+                        echo "Oops! Something went wrong. Please try again later.";
                       }
-                    
+                  
                     // Close connection
                     unset($pdo);
                     ?>
